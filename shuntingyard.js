e522055ac70e20984b8cc5672df5edb0a898d8b2
Guillaume Daniel <g.daniel.buca@gmail.com>
2022-05-06 08:35:01 +0200

    first draft: late night code might be turbo bugged

---
+++ shuntingyard.js
@@ -0,0 +1,191 @@
+const associativity = {
+    "^": "right",
+    "*": "left",
+    "/": "left",
+    "+": "left",
+    "-": "left"
+};
+
+const precedence = {
+    "^": 4,
+    "*": 3,
+    "/": 3,
+    "+": 2,
+    "-": 2
+};
+
+class Token {
+    constructor(type, value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    precedence = () => { return precedence[this.value]; };
+
+    associativity = () => { return associativity[this.value]; };
+}
+
+
+// cheap tokenizer
+// assumes valid expression
+function tokenizeExpression(input) {
+    let tokens = [];
+    let literalBuffer = "";
+
+    const flushLiteral = () => {
+        if (literalBuffer.length > 0) {
+            tokens.push(new Token("LITERAL", literalBuffer));
+            literalBuffer = "";
+        }
+    }
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] == ' ' || input[i] == '\n' || input[i] == '\t')
+            break;
+        switch (input[i]) {
+            case '(':
+                flushLiteral();
+                tokens.push(new Token("PARENTHESIS_OPEN", "("));
+                break;
+            case ')':
+                flushLiteral();
+                tokens.push(new Token("PARENTHESIS_CLOSE", ")"));
+                break;
+            case '+':
+                flushLiteral();
+                tokens.push(new Token("OPERATOR_ADD", "+"));
+                break;
+            case '-':
+                flushLiteral();
+                tokens.push(new Token("OPERATOR_SUB", "-"));
+                break;
+            case '*':
+                flushLiteral();
+                tokens.push(new Token("OPERATOR_MUL", "*"));
+                break;
+            case '/':
+                flushLiteral();
+                tokens.push(new Token("OPERATOR_DIV", "/"));
+                break;
+            case '^':
+                flushLiteral();
+                tokens.push(new Token("OPERATOR_POW", "^"));
+            default:
+                literalBuffer += input[i];
+                if (i == input.length - 1)
+                    flushLiteral();
+                break;
+        }
+    }
+
+    return tokens;
+}
+
+// we need a basic Stack.peek
+function peek(a) {
+    return a[a.length - 1];
+}
+
+// here I implement shunting yard as per wikipedia
+// https://en.wikipedia.org/wiki/Shunting_yard_algorithm
+function parse(input) {
+    let operators_stack = [];
+    let output_queue = [];
+
+    let tokens = tokenizeExpression(input);
+
+    let i = 0;
+
+    // start of shunting yard
+    tokens.forEach(token => {
+        switch (token.type) {
+            case "LITERAL":
+                output_queue.push(token);
+                break;
+            case "FUNCTION":
+                operators_stack.push(token);
+                break;
+            case "PARENTHESIS_OPEN":
+                operators_stack.push(token);
+                break;
+            case "PARENTHESIS_CLOSE":
+                {
+                    while (peek(operators_stack).type != "PARENTHESIS_OPEN") {
+                        if (operators_stack.length == 0) {
+                            console.error("missing symbol : (");
+                            return;
+                        } else {
+                            output_queue.push(operators_stack.pop())
+                        }
+                    }
+                    // remove matching '('
+                    operators_stack.pop();
+                    if (operators_stack.at(-1).type == "FUNCTION")
+                        output_queue.push(operators_stack.pop());
+                }
+                break;
+            default: // it'a an operator
+                {
+                    let top = operators_stack.at(-1);
+                    while (typeof top !== "undefined" && top.type != "PARENTHESIS_OPEN" && (token.precedence() < top.precedence() || (token.precedence() == top.precedence()) && token.associativity() == "left")) {
+                        output_queue.push(operators_stack.pop());
+                    }
+                    operators_stack.push(token);
+                }
+                break;
+        }
+        i++;
+    });
+
+    // pop remaining operators to the output queue
+    operators_stack.forEach(() => {
+        let top = peek(operators_stack);
+        if (typeof top !== "undefined" && top.type == "PARENTHESIS_OPEN") {
+            console.error("missmatched parenthesis !");
+            return;
+        }
+        output_queue.push(operators_stack.pop());
+    });
+
+    return output_queue.concat(operators_stack.reverse());
+}
+
+class AST {
+    constructor(token, leftChildNode, rightChildNode) {
+        this.token = token.value;
+        this.leftChildNode = leftChildNode;
+        this.rightChildNode = rightChildNode;
+    }
+}
+
+function evaluateRpn(expr) {
+    let stack = [];
+
+    for (let i = 0; i < expr.length; i++) {
+        if (!isNaN(expr[i]) && isFinite(expr[i])) {
+            stack.push(expr[i]);
+        } else {
+            let a = stack.pop();
+            let b = stack.pop();
+            if (expr[i] === "+") {
+                stack.push(parseInt(a) + parseInt(b));
+            } else if (expr[i] === "-") {
+                stack.push(parseInt(b) - parseInt(a));
+            } else if (expr[i] === "*") {
+                stack.push(parseInt(a) * parseInt(b));
+            } else if (expr[i] === "/") {
+                stack.push(parseInt(b) / parseInt(a));
+            } else if (expr[i] === "^") {
+                stack.push(Math.pow(parseInt(b), parseInt(a)));
+            }
+        }
+    }
+
+    return stack.length > 1 ? "ERROR" : stack[0];
+}
+
+function calculette(input) {
+    let rpn = parse(input);
+    let result = evaluateRpn(rpn.map(e => e.value));
+    console.log(result);
+}
